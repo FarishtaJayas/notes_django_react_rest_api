ca5ef7edaf2acf25fd2c7e4439fd757b5c532966
FarishtaJayas <farishtajayas@gmail.com>
2023-11-06 03:13:30 +0600

    hash router solves django not handling the links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
 import NotePage from './pages/NotePage'
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="container dark">
-        <div className='app'>
-        <Header />
-        <Routes>
-          <Route path="/" element={<NotesListPage />} />
-          <Route path="note/:id" element={<NotePage />}/>
-        </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <Router>
+            <div className="container dark">
+                <div className='app'>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<NotesListPage/>}/>
+                        <Route path="note/:id" element={<NotePage/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
